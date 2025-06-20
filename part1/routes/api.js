1b02b5f59fe7eaa17cc33c475dd945ba12481dab
Kate Osborne <kate.osborne@student.adelaide.edu.au>
2025-06-20 02:04:48 +0000

    [automatic commit] 2025-06-20 02:04:48 UTC 202.7.247.251

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -23,5 +23,23 @@ router.get('/dogs', function(req, res, next) {
     });
 });
 
+router.get('/dogs', function(req, res, next) {
+    req.pool.getConnection(function(err, connection) {
+        if (err) {
+            res.sendStatus(500);
+            return;
+        }
+        var query = 'SELECT name, size, username AS owner_username FROM Dogs AS d JOIN Users AS u ON d.owner_id = u.user_id;';
+        connection.query(query, function(err, rows, fields) {
+            connection.release();
+            if (err) {
+                res.sendStatus(500);
+                return;
+            }
+            res.json(rows);
+        });
+    });
+});
+
 module.exports = router;
 
