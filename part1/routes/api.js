e558de48e83d4c370cf613f5b29b306f7926e51d
Kate Osborne <kate.osborne@student.adelaide.edu.au>
2025-06-20 02:26:00 +0000

    [automatic commit] 2025-06-20 02:26:00 UTC 202.7.247.251

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var db = 
+var db = require('../db');
 
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
