d640009eaabf09b805ce132fefd28cbe006b97bb
Kate Osborne <kate.osborne@student.adelaide.edu.au>
2025-06-20 05:14:06 +0000

    [automatic commit] 2025-06-20 05:14:06 UTC 202.7.247.251

--- part1/public/javascripts/index.js
+++ part1/public/javascripts/index.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
     methods: {
         click_button() {
             this.button_text = "Now you can sign up at the top of the page with that button";
+            document.getElementById("real-signup").
         }
     },
     mounted() {
