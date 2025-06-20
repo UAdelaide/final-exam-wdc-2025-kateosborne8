7a1b8cec110e887010cc22461e9160d88b2b773b
Kate Osborne <kate.osborne@student.adelaide.edu.au>
2025-06-20 10:53:21 +0000

    [automatic commit] 2025-06-20 10:53:21 UTC 202.7.247.251

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,6 +49,10 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
+    req.session.user = {
+      user_id: 
+    }
+
     res.json({ message: 'Login successful', user: rows[0].username });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
