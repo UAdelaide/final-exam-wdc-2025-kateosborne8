1f52b9af0a48c4bce5bb4d47f5ffce7f3886c760
Kate Osborne <kate.osborne@student.adelaide.edu.au>
2025-06-20 13:26:00 +0000

    [automatic commit] 2025-06-20 13:26:00 UTC ;; communications error to 208.67.222.222#53: timed out 202.7.247.251

--- part1/db.js
+++ part1/db.js
@@ -52,6 +52,31 @@ var db = mysql.createPool({
           ('jacob7', 'jacob@example.com', 'hashed700', 'walker');
         `);
       }
+
+      // Create a table if it doesn't exist
+      await db.execute(`
+        CREATE TABLE IF NOT EXISTS Dogs (
+          dog_id INT AUTO_INCREMENT PRIMARY KEY,
+          username VARCHAR(50),
+          email VARCHAR(50),
+          password_hash varchar(255),
+          role enum('owner', 'walker')
+          created_at timestamp
+        )
+      `);
+
+      // Insert data if table is empty
+      const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
+      if (rows[0].count === 0) {
+        await db.execute(`
+          INSERT INTO Users (username, email, password_hash, role) VALUES
+          ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+          ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+          ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+          ('kateo8', 'kate@example.com', 'hashed888', 'owner'),
+          ('jacob7', 'jacob@example.com', 'hashed700', 'walker');
+        `);
+      }
     } catch (err) {
       console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
     }
