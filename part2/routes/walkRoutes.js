014b107f11a7774eef43753454f51e51c5f9e382
Kate Osborne <kate.osborne@student.adelaide.edu.au>
2025-06-20 12:52:30 +0000

    [automatic commit] 2025-06-20 12:52:29 UTC 202.7.247.251

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -28,7 +28,7 @@ router.post('/', async (req, res) => {
     const [result] = await db.query(`
       INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location)
       VALUES (?, ?, ?, ?)
-    `, [dog_id.dog_id, requested_time, duration_minutes, location]);
+    `, [dog_id[0].dog_id, requested_time, duration_minutes, location]);
 
     res.status(201).json({ message: 'Walk request created', request_id: result.insertId });
   } catch (error) {
