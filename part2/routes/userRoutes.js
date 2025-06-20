8c90d91af3f0aef1c0147390f24c32f44759cefc
Kate Osborne <kate.osborne@student.adelaide.edu.au>
2025-06-20 07:49:15 +0000

    [automatic commit] 2025-06-20 07:49:14 UTC 202.7.247.251

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -64,7 +64,7 @@ const { username, password } = req.body;
     }
 
     req.pool.query(
-        'SELECT password FROM Users WHERE username = ?',
+        'SELECT password_hash FROM Users WHERE username = ?',
         [username],
         async function (err, results) {
             if (err) {
