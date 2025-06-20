8a15c7ffd43a0ae753224f60e5073429956c1644
Kate Osborne <kate.osborne@student.adelaide.edu.au>
2025-06-20 02:13:17 +0000

    [automatic commit] 2025-06-20 02:13:17 UTC 202.7.247.251

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -41,5 +41,23 @@ router.get('/walkrequests/open', function(req, res, next) {
     });
 });
 
+router.get('/walkers/summary', function(req, res, next) {
+    req.pool.getConnection(function(err, connection) {
+        if (err) {
+            res.sendStatus(500);
+            return;
+        }
+        var query = 'SELECT wr.request_id, d.name AS dog_name, requested_time, duration_minutes, location, u.username AS owner_username FROM WalkRequests AS wr JOIN Dogs AS d ON wr.dog_id = d.dog_id JOIN Users AS u ON u.user_id = d.owner_id WHERE wr.status="open";';
+        connection.query(query, function(err, rows, fields) {
+            connection.release();
+            if (err) {
+                res.sendStatus(500);
+                return;
+            }
+            res.json(rows);
+        });
+    });
+});
+
 module.exports = router;
 
