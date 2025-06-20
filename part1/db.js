6d65ecf20f066c0b1259f87ca5d001a0cdf9bb18
Kate Osborne <kate.osborne@student.adelaide.edu.au>
2025-06-20 13:23:43 +0000

    [automatic commit] 2025-06-20 13:23:43 UTC 202.7.247.251

--- part1/db.js
+++ part1/db.js
@@ -32,8 +32,8 @@ var db = mysql.createPool({
       await db.execute(`
         CREATE TABLE IF NOT EXISTS Users (
           user_id INT AUTO_INCREMENT PRIMARY KEY,
-          title VARCHAR(255),
-          author VARCHAR(255)
+          username VARCHAR(50),
+          email VARCHAR(50)
         )
       `);
 
