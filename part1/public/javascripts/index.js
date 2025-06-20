afc028fe582b3701453c3a27c43dbe8a8380c920
Kate Osborne <kate.osborne@student.adelaide.edu.au>
2025-06-20 04:55:48 +0000

    [automatic commit] 2025-06-20 04:55:48 UTC 202.7.247.251

--- part1/public/javascripts/index.js
+++ part1/public/javascripts/index.js
@@ -10,26 +10,27 @@ var vueinst = new Vue({
             return 'blah blah';
         }
     },
-    mounted
+    mounted() {
+        async function getDoggo() {
+            try {
+                const response = await fetch('https://dog.ceo/api/breeds/image/random');
+                const data = await response.json();
+                return data.message;
+            } catch (err) {
+                /* Oh no! No doggo! */
+                console.log("Failed to retrieve a doggo...");
+                return null;
+            }
+        }
+
+        async function showDoggo() {
+            const url = await getDoggo();
+            if (url) {
+                document.getElementById("doggo-otd").setAttribute("src", url);
+            }
+        }
+
+        showDoggo();
+    }
 });
 
-async function getDoggo() {
-    try {
-        const response = await fetch('https://dog.ceo/api/breeds/image/random');
-        const data = await response.json();
-        return data.message;
-    } catch (err) {
-        /* Oh no! No doggo! */
-        console.log("Failed to retrieve a doggo...");
-        return null;
-    }
-}
-
-async function showDoggo() {
-    const url = await getDoggo();
-    if (url) {
-        document.getElementById("doggo-otd").setAttribute("src", url);
-    }
-}
-
-showDoggo();
