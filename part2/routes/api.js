da356f4777bba6dc375b289898413d8c1769ea9a
Kate Osborne <kate.osborne@student.adelaide.edu.au>
2025-06-20 10:09:41 +0000

    [automatic commit] 2025-06-20 10:09:41 UTC ;; communications error to 208.67.222.222#53: timed out 202.7.247.251

---
+++ part2/routes/api.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../db');
+
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.get('/dogs', async(req, res) => {
+    try {
+        const [rows] = await db.query('SELECT name, size, username AS owner_username FROM Dogs AS d JOIN Users AS u ON d.owner_id = u.user_id;');
+        res.json(rows);
+    } catch (err) {
+        console.error("Database Error: ", err);
+        res.status(500).send("Failed to retreive data from database.");
+    }
+
+});
+
+router.get('/walkrequests/open', async(req, res) => {
+    try {
+        const [rows] = await db.query('SELECT wr.request_id, d.name AS dog_name, requested_time, duration_minutes, location, u.username AS owner_username FROM WalkRequests AS wr JOIN Dogs AS d ON wr.dog_id = d.dog_id JOIN Users AS u ON u.user_id = d.owner_id WHERE wr.status="open";');
+        res.json(rows);
+    } catch (err) {
+        console.error("Database Error: ", err);
+        res.status(500).send("Failed to retreive data from database.");
+    }
+
+});
+
+router.get('/walkers/summary', async(req, res) => {
+    try {
+        const [rows] = await db.query('SELECT u.username AS walker_username, COUNT(DISTINCT rate.rating_id) AS total_ratings, ROUND(AVG(rate.rating),1) AS average_rating, COUNT(DISTINCT wa.request_id) AS completed_walks FROM WalkRatings AS rate JOIN Users AS u ON rate.walker_id = u.user_id JOIN WalkApplications AS wa ON wa.walker_id = u.user_id JOIN WalkRequests as wr ON wa.request_id = wr.request_id WHERE wa.status="accepted" AND wr.status="completed" GROUP BY rate.walker_id;');
+        res.json(rows);
+    } catch (err) {
+        console.error("Database Error: ", err);
+        res.status(500).send("Failed to retreive data from database.");
+    }
+
+});
+
+module.exports = router;
+
