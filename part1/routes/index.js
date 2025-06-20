a138d57a5dc75f41d16b4ffddfc4194cca20e79d
Kate Osborne <kate.osborne@student.adelaide.edu.au>
2025-06-20 01:58:01 +0000

    [automatic commit] 2025-06-20 01:58:01 UTC 202.7.247.251

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/something', function(req, res, next) {
+  req.pool.getConnection(function(err, connection) {
+      if (err) {
+          res.sendStatus(500);
+          return;
+      }
+      var query = 'SELECT * FROM Dogs;';
+      connection.query(query, function(err, rows, fields) {
+          connection.release();
+          if (err) {
+              res.sendStatus(500);
+              return;
+          }
+          res.json(rows);
+      });
+  });
+});
+
 module.exports = router;
